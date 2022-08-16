a681ae0d642d048bdeea802d5c0c116a908a89b9
Anton <antonzakhlebayeu@gmail.com>
2022-08-17 01:00:11 +0300

    Connect backend for authorization with coinbase

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 function HomePage() {
-  return <div></div>;
+  const handleConnectCoinbase = () => {
+    if (process.env.REACT_APP_COINBASE_AUTH_URI) {
+      window.location.href = process.env.REACT_APP_COINBASE_AUTH_URI;
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center flex-col h-screen">
+      <Button variant="contained" size="large" onClick={handleConnectCoinbase}>
+        Connect coinbase
+      </Button>
+    </div>
+  );
 }
 
 export default HomePage;
